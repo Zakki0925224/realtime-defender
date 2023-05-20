297bb544c645dcee3c3be90be24cc302920e4856
Zakki <zakki0925224@gmail.com>
2023-05-20 17:27:03 +0900

    [Add]: Added ELF parser

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -1,5 +1,6 @@
 use std::{fs::read, path::PathBuf};
 
+use elf::{endian::AnyEndian, ElfBytes};
 use file_format::FileFormat;
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -10,6 +11,7 @@ pub enum AnalyzedLevel {
 }
 
 pub struct Analyzer {
+    file_bytes: Vec<u8>,
     analyzed_level: AnalyzedLevel,
     analyzing_filepath: PathBuf,
     file_format: FileFormat,
@@ -19,6 +21,7 @@ pub struct Analyzer {
 impl Analyzer {
     pub fn new() -> Self {
         return Self {
+            file_bytes: Vec::new(),
             analyzed_level: AnalyzedLevel::None,
             analyzing_filepath: PathBuf::new(),
             file_format: FileFormat::default(),
@@ -60,8 +63,8 @@ impl Analyzer {
             }
         };
 
-        let bytes = read(filepath).unwrap();
-        let byte_slice = bytes.as_slice();
+        self.file_bytes = read(&self.analyzing_filepath).expect("Failed to read file");
+        let byte_slice = self.file_bytes.as_slice();
         self.sha256_hash = sha256::digest(byte_slice);
 
         self.analyzed_level = AnalyzedLevel::Heuristic;
@@ -71,6 +74,21 @@ impl Analyzer {
         let filepath = &self.analyzing_filepath;
 
         info!("Analyzing statically: \"{}\"...", filepath.display());
+
+        match self.file_format {
+            FileFormat::ExecutableAndLinkableFormat => (),
+            _ => unimplemented!("Support ELF file only"),
+        }
+
+        // analyze ELF file
+        let byte_slice = self.file_bytes.as_slice();
+        let elf = match ElfBytes::<AnyEndian>::minimal_parse(byte_slice) {
+            Ok(elf) => elf,
+            Err(_) => {
+                warn!("Failed to parse ELF file");
+                return;
+            }
+        };
 
         self.analyzed_level = AnalyzedLevel::Static;
     }
